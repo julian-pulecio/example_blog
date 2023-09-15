a69f6099074d0f27ab9ab1d8f69a4d3d48cd6ebd
julian pulecio <developer@julianpulecio.com>
2023-09-15 12:00:46 -0500

    features: test for Post Model and PostList view are added

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
-class Blog(models.Model):
-    
+class Post(models.Model):
     class Status(models.TextChoices):
         PUBLISHED = ('PB','Published')
         DRAFT = ('DF','Draft')
@@ -13,6 +14,9 @@ class Blog(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
     status = models.CharField(choices=Status.choices, default=Status.DRAFT, max_length=2)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    slug = models.SlugField(max_length=255)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+    paginate_by = 10
+    template_name = 'post/post_list.html'
