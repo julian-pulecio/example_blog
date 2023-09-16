a0fbcb6d1193a3701e518e61cce86425ef8fe9f7
julian pulecio <developer@julianpulecio.com>
2023-09-16 15:28:30 -0500

    feature: test for Post detail view are added

--- tests/blog/test_views.py
+++ tests/blog/test_views.py
@@ -3,13 +3,20 @@ from django.urls import reverse
 from blog.factory import PostFactory
 
 @pytest.fixture()
+def setup_one_item():
+    return PostFactory.create(
+        title = 'post title',
+        sub_title = 'post subtitle',
+        content = 'post content',
+    )
+
+@pytest.fixture()
 def setup_five_items():
     _ = PostFactory.create_batch(5)
 
 @pytest.fixture()
 def setup_one_hundred_items():
     _ = PostFactory.create_batch(100)
-
 
 @pytest.mark.django_db
 class TestPostListView:
@@ -24,24 +31,23 @@ class TestPostListView:
     def test_view_pagination_for_five_post(self, client, setup_five_items):
         response = client.get(reverse('blog.list'), {'page': '1'})
         assert len(response.context.get('object_list')) == 5
-        assert 'Page 1 of 1' in response.content.decode()
-        assert 'next' not in response.content.decode()
-        assert 'previous' not in response.content.decode()
     
     def test_view_pagination_for_one_hundred_post(self, client, setup_one_hundred_items):
         response = client.get(reverse('blog.list'), {'page': '2'})
         assert len(response.context.get('object_list')) == 10
-        assert 'Page 2 of 10.' in response.content.decode()
-        assert 'next' in response.content.decode()
-        assert 'previous' in response.content.decode()
 
-        response = client.get(reverse('blog.list'), {'page': '1'})
-        assert 'Page 1 of 10.' in response.content.decode()
-        assert 'next' in response.content.decode()
-        assert 'previous' not in response.content.decode()
-
-        response = client.get(reverse('blog.list'), {'page': '10'})
-        assert 'Page 10 of 10.' in response.content.decode()
-        assert 'next' not in response.content.decode()
-        assert 'previous' in response.content.decode()
-        
+@pytest.mark.django_db
+class TestPostDetailView:
+    def test_view_returns_200_status_code(self, client, setup_one_item):
+        response = client.get(reverse('blog.detail', args=[setup_one_item.slug]))
+        assert response.status_code == 200
+    
+    def test_view_returns_404_status_code_when_post_not_found(self, client):
+        response = client.get(reverse('blog.detail', args=['non-exist-post']))
+        assert response.status_code == 404
+    
+    def test_view_render_content_correctly(self, client, setup_one_item):
+        response = client.get(reverse('blog.detail', args=[setup_one_item.slug]))
+        assert setup_one_item.title in response.rendered_content
+        assert setup_one_item.sub_title in response.rendered_content
+        assert setup_one_item.content in response.rendered_content
